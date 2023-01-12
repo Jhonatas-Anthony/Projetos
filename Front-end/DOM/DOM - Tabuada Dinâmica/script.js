e88c03a386100934358fee6696af7c807e08dd30
JhonatasHellw <jhonatasanthony6@gmail.com>
2023-01-11 23:30:35 -0300

    tabuada

--- Front-end/DOM/DOM - Tabuada Dinâmica/script.js
+++ Front-end/DOM/DOM - Tabuada Dinâmica/script.js
@@ -1,11 +1,36 @@
 function tabuada(){
+
+
     let numero = Number((document.querySelector('input#start')).value)
+    let div = document.querySelector('#formDiv')
+    let button = document.querySelector('#criar')
     let resultado = document.querySelector('#resultado')
-    resultado.size = resultado.length
-    
+    let optTemporaria = document.querySelector('#temp')
+
+    resultado.removeChild(optTemporaria)
+    resultado.setAttribute('size', '10')
 
     for (let i = 1 ; i <= 10 ; i++){
-        // Create Element para criar as options 
-        resultado.options[resultado.options.length] = new Option(`${numero} * ${i} = ${numero * i}`)
+        let newOpt = document.createElement('option')
+
+        let texto = `${numero} * ${i} = ${numero * i}`
+
+        resultado.appendChild(newOpt)
+
+        newOpt.setAttribute('value', `${i}`)
+
+        newOpt.innerHTML = texto
+
+        //Estudar a seguinte alternativa em casa:
+
+        /* resultado.options[resultado.options.length] = new Option(`${numero} * ${i} = ${numero * i}`) */
     }
+
+    let refresh = document.createElement('button')
+    div.removeChild(button)
+
+    refresh.setAttribute("onclick", "window.location.reload()")
+    refresh.innerHTML = "Novo Cálculo"
+    div.appendChild(refresh)
+    
 }
